515c802eb1abca4d9b878d2aa9a97f759cfc303a
Murat <89196242960@mail.ru>
2020-05-04 16:37:02 +0300

    add trick generation logic

--- Wins/RootVC/GameView/GameViewInteractor.swift
+++ Wins/RootVC/GameView/GameViewInteractor.swift
@@ -11,10 +11,12 @@ import Foundation
 class GameViewInteractor{
     weak var output: GameViewInteractorOutput!
     
+    var skateTricks = [Trick]()
+    
     func startConfigure(){
         let actulChallenge = DataManager._shared.actualChallenges
-        let allTricks = DataManager._shared.skateTricks
-        let tenTrick = GameViewInteractor.rundomTrick(tricks: allTricks, maxNumber: 10)//Написть логику
+        self.skateTricks = DataManager._shared.skateTricks
+        let tenTrick = self.generateTenTricks()
         self.output.configure(with: tenTrick, actulChallenge)
     }
     
@@ -28,6 +30,109 @@ class GameViewInteractor{
             tricksForGame.append(trick)
         }
         return tricksForGame
+    }
+    
+    func generateTenTricks() -> [Trick]{
+        var arr = [Trick]()
+        arr.append(self.generateTricks(maxStab: 10, minStab: 7, maxDif: 0.3))   //1
+        arr.append(self.generateTricks(maxStab: 7, minStab: 5, maxDif: 0.4))    //2
+        arr.append(self.generateTricks(maxStab: 5, minStab: 4, maxDif: 0.6))    //3
+        arr.append(self.generateTricks(maxStab: 4, minStab: 3, maxDif: 0.7))    //4
+        arr.append(self.generateTricks(maxStab: 3, minStab: 2, maxDif: 0.8))    //5
+        arr.append(self.generateTricks(maxStab: 2, minStab: 1, maxDif: 0.9))    //6
+        arr.append(self.rundomTrick())    //7
+        arr.append(self.rundomTrick())    //8
+        arr.append(self.rundomTrick())    //9
+        arr.append(self.rundomTrick())    //10
+        self.skateTricks = DataManager._shared.skateTricks
+        return arr
+    }
+    
+//    1. Стабильность от 7 среди выполненных трюков с максимальной сложностью, если таких трюков нет, тогда выдавать трюк с 30% сложностью от максимального трюка который умеешь, если такого трюка нет, то любой трюк с сложностью не выше который умеешь
+//    2. Стабильность от 5 среди выполненных трюков с максимальной сложностью, если таких трюков нет, тогда вызывать трюк с 40% сложность от максимального трюка который умеешь, то любой трюк с сложностью не выше который умеешь
+//    3. Стабильность от 4 среди выполненных трюков с максимальной сложностью, если таких трюков нет, тогда вызывать трюк с 60% сложности от максимального трюка который умеешь, если нет, то любой трюк с сложностью не выше твоей
+//    4. Стабильность от 3 среди выполненных трюков с максимальной сложностью, если таких трюков нет, тогда вызывать трюк с 70% сложности от максимального трюка который умеешь, если нет, то любой трюк с сложностью не выше твоей
+//    5. Стабильность от 2 среди выполненных трюков с максимальной сложностью, если таких трюков нет, тогда вызывать трюк с 80% сложности от максимального трюка который умеешь, если нет, то любой трюк с сложностью не выше твоей
+//    6. Стабильность от 1 среди выполненных трюков с максимальной сложностью, если таких трюков нет, тогда вызывать трюк с 90% сложности от максимального трюка который умеешь, если нет, то любой трюк с сложностью не выше твоей
+//    7. Не умеешь трюк но сложность равна средней сложности от трюков которые умеешь, если таких трюков нет, то любой трюк который не умеешь с сложностью не больше сложности трюков которые умеешь
+//    8. Не умеешь трюк но сложность равно 80% от максимальной сложности которую умеешь, если таких трюков нет, то любой трюк который не умеешь с сложность/ не большое сложности трюков которые умеешь
+//    9. Любой трюк
+//    10. Трюк который не умеешь и сложность трюка которого больше сложности трюка который умеешь
+
+    func generateTricks(maxStab: Int, minStab: Int, maxDif: Float) -> Trick {
+        //выбираем трюк по стабильости
+        let stabTricks = self.skateTricks.filter{$0.stability >= minStab && $0.stability <= maxStab}
+        guard stabTricks.isEmpty else { return self.defaultTrick(with: maxDif) }
+        //если трюки по стаибльности найдены, то выбираем трюк с максимальной сложностью
+        let trickDif = self.maxDifTrick(tricks: stabTricks)
+        
+        guard trickDif != nil else { return self.defaultTrick(with: maxDif) }
+        
+        let index = self.skateTricks.firstIndex(of: trickDif!)!
+        self.skateTricks.remove(at: index)
+        
+        return trickDif!
+    }
+    
+    func defaultTrick(with dif: Float) -> Trick{
+        //если такого трюка нет то выбираем трюк  с сложность по близкий к опреде сложности
+        let trick = self.trickByDif(dif: dif)
+        if trick == nil{
+            //если и такого трюка нет, то на рандом
+            return self.rundomTrick()
+        }else{
+            let index = self.skateTricks.firstIndex(of: trick!)!
+            self.skateTricks.remove(at: index)
+            return trick!
+        }
+    }
+    
+    //aлгоритм находящий трюк с максимальной сложности из имеющихся
+    func maxDifTrick(tricks: [Trick]) -> Trick?{
+        var maxDif: Float = 0
+        var trickDif: Trick? = nil
+        for trick in tricks{
+            guard trick.complexity > maxDif else { continue}
+            maxDif = trick.complexity
+            trickDif = trick
+        }
+    
+        return trickDif
+    }
+    
+    //алгоритм находящий близки по сложности трюк из имеющихся
+    func trickByCurrentDif(tricks: [Trick], dif: Float) -> Trick?{
+        let maxDifTrick = self.maxDifTrick(tricks: tricks)
+        guard let trickDif = maxDifTrick else { return nil}
+        let maxDif = trickDif.complexity * dif
+        var diferent: Float = 10
+        var trickCurrentDif: Trick? = nil
+        for trick in tricks{
+            let difrnt = trick.complexity - maxDif
+            if difrnt < diferent{
+                diferent = difrnt
+                trickCurrentDif = trick
+            }
+        }
+        return trickCurrentDif
+    }
+    
+    //трюк находящий трюк с близкой сложность
+    func trickByDif(dif: Float) -> Trick?{
+        let learnedTrick = self.skateTricks.filter{$0.stability >= 1}
+        let difTrick = trickByCurrentDif(tricks: learnedTrick, dif: dif)
+        if difTrick == nil{
+            return self.maxDifTrick(tricks: learnedTrick)
+        }else{
+            return difTrick
+        }
+    }
+    
+    func rundomTrick() -> Trick{
+        let index = Int.random(in: 0...(self.skateTricks.count - 1))
+        let trick = self.skateTricks[index]
+        self.skateTricks.remove(at: index)
+        return trick
     }
 }
 
@@ -53,6 +158,5 @@ extension GameViewInteractor: GameViewInteractorInput{
         
         DataManager._shared.saveTechnikalSkill(techSkill)
     }
-    
-    
 }
+
