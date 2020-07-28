bdc5b1a46478ab335fe1628560c9e1342e8f2f68
Murat <89196242960@mail.ru>
2020-07-29 00:43:49 +0300

    Promocode bug fix

--- Wins/DoneChalengeVC/DoneChallengeRouter.swift
+++ Wins/DoneChalengeVC/DoneChallengeRouter.swift
@@ -13,8 +13,13 @@ class DoneChallengeRouter: DoneChallengeRouterInput {
     weak var view: UIViewController!
     
     func showPromocode(with promocode: String?) {
-
-        let alert = UIAlertController(title: "Promocode:", message: promocode ?? "Without promocode(", preferredStyle: .actionSheet)
+        guard let promocode = promocode else{
+            let alert = UIAlertController(title: "It's challenge without promocode", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+            self.view.present(alert, animated: true)
+            return
+        }
+        let alert = UIAlertController(title: "Promocode:", message: promocode, preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "Coppy", style: .default, handler: { (_) in
             UIPasteboard.general.string = promocode
         }))
